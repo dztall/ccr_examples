cb5e7e6f7ae84f4536c15d44a2be1c6b44b8da7d
Lee Jeong Seop <dztall@hanmail.net>
2018-07-02 08:54:20 +0900

    python pickle example added

---
+++ Languages/Python3/using_pickle.py
@@ -0,0 +1,14 @@
+# Original source code found at : https://wiki.python.org/moin/UsingPickle
+
+# Save a dictionary into a pickle file.
+import pickle
+
+favorite_color = { "lion": "yellow", "kitty": "red" }
+
+pickle.dump( favorite_color, open( "save.p", "wb" ) )
+
+# Load the dictionary back from the pickle file.
+favorite_color_loaded = pickle.load( open( "save.p", "rb" ) )
+# favorite_color_loaded is now { "lion": "yellow", "kitty": "red" }
+print(favorite_color_loaded)
+
