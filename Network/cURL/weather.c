e4ca64f9cbea0c7f03666dc360fc1eeae101a0a2
Lee Jeong Seop <dztall@hanmail.net>
2018-07-09 09:30:14 +0900

    weather.c added

---
+++ Network/cURL/weather.c
@@ -0,0 +1,17 @@
+﻿//Original source code found at : https://theptrk.com/2018/07/08/weather-reports-with-curl/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char city[512] = "Seoul";
+	printf("Your city? ");
+	scanf("%s", city);
+
+	char command[512] = "";
+	sprintf(command, "curl wttr.in/~%s?0", city);
+	system(command);
+
+	return 0;
+}
